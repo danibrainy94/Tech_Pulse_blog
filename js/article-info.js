7f517ddcf3ad71daf5defb9cb584ba75dbabee0b
danibrainy94 <danibrainy94@gmail.com>
2025-08-29 15:31:08 +0100

    master

--- js/article-info.js
+++
@@ -1,6 +0,0 @@
-// open up the articles page at the click of the title
-
-$('#postsGrid').on('click', '.post-title', function(e) {
-    e.preventDefault();
-    window.location.href = './article-info.html';
-});
